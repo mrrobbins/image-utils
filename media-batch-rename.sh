359f93381547b735caabc2a3c855b95be3aa035e
Matthew Robbins <matthew.r.robbins@gmail.com>
2017-06-27 19:43:47 -0400

    Updated script manual

--- media-batch-rename.sh
+++ media-batch-rename.sh
@@ -4,7 +4,7 @@
 # Output files to subdirectory named "media-batch-renamed". Original files are preserved.
 # Process the files in ascending order. Sorted by DateTimeOriginal tag, then CreateDate tag, then Filename.
 
-# This script favors a date formatted filename. For each file, it attempts the following renaming strategies, based on the available metadata tags in the file, where last-one-wins:
+# This script favors a date formatted filename. For each file, it attempts the following renaming strategies, in order, based on the available metadata tags in the file, where last-one-wins:
 ## Strategy 1: Simple, sequential zero-based index. Max 1000 files.
 ### Format: ###_0.extension
 ### Example: "000_0.jpg", "001_0.jpg", ..., "999_0.jpg"
@@ -19,11 +19,11 @@
 
 # Note: the two-digit field "_##_" in the date formats, after the seconds field.
 ## This field is for uniquely naming burst shots taken within the same second, using a sequential zero-based index.
-## This approach is a substitute for a proper sub-second time field. The DateTimeOriginal tag does not record sub-second time and including it is optional and there is no standardized tag or value format used by manufactures.
+## This approach is a substitute for a proper sub-second (e.g. millisecond) time field. The DateTimeOriginal tag does not record sub-second time. Including other tags for recording sub-second time is optional and non-standard between vendors.
 ## Example: "2017-01-01_00:00:00_00_0.jpg", "2017-01-01_00:00:00_01_0.jpg" -- e.g. two burst shots of a New Years kiss, both taken in the first second after midnight 2017.
 
-# Note: the trailing "_0" in the filename, before the extension. This is a legacy digit meant to handle duplicate files. E.g. copies or edits of the same (captured) jpeg image.
-# Removing this field and only adding it manually, on an as-needed basis, is possible.
+# Note: the trailing "_0" in the filename, before the extension. This is a legacy digit meant to handle duplicate files. E.g. copies or edits of an original jpeg image.
+# Omitting this field and only adding it manually, on an as-needed basis, is possible.
 # However, lexical sorts (e.g. by a file explorer) will place the duplicate (longer filename) BEFORE the original, which is annoying.
 # E.g. "foobar_1.jpg" or "foobar_edited.jpg" comes before "foobar.jpg"
 # The standardized filename length and format used here will prevent this.
